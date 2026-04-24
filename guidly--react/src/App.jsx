9248e4c348d030b1ad7c8609cfe441ce14a3cb62
fatimazahraeben <fatimazahraebenkhadda07@gmail.com>
2026-04-24 10:53:49 +0100

    Modification du projet

--- guidly--react/src/App.jsx
+++ guidly--react/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PagePrincipale from "./components/PagePrincipale";
-import AuthPage from "./components/AuthPage";
+import Home from "./components/Home";
+import AuthPage from './components/AuthPage';
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<PagePrincipale />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/auth" element={<AuthPage />} />
             </Routes>
         </Router>
